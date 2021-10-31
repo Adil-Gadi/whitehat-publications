7080e71ac21400b93174bb2765e61b6bd65bc5fb
Adil Gadi <adil@gadifamily.com>
2021-10-30 17:53:23 -0700

    Project 30

--- TowerSiege-1-main/models/Box.js
+++ TowerSiege-1-main/models/Box.js
@@ -1,7 +1,24 @@
 class Box extends BaseClass {
     constructor(x, y, width, height) {
         super(x, y, width, height);
+        this.visibility = true;
+        this.width = 50;
+        this.height = 50;
         // this.image = loadImage("sprites/wood1.png");
     }
 
+    display() {
+        if (this.body.speed < 3.8) {
+            super.display();
+        } else {
+            World.remove(world, this.body);
+            push();
+            this.visibility = this.visibility - 5;
+            tint(255, this.visibility);
+            rectMode(CENTER);
+            rect(0, 0, this.width, this.height);
+            pop();
+        }
+        // super.display();
+    }
 };
--- TowerSiege-1-main/sketch.js
+++ TowerSiege-1-main/sketch.js
@@ -6,7 +6,7 @@ const Constraint = Matter.Constraint;
 var engine, world;
 var ground, polygon, sling;
 var platform1, platform2, platform3;
-var blocks = [];
+var box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13, box14, box15, box16, box17, box18;
 
 function preload() {
 
@@ -25,13 +25,26 @@ function setup() {
     polygon = new Polygon(100, 600, 50, 50);
     sling = new Sling(polygon.body, { "x": 75, "y": 500 });
 
-    for (var i = 100; i <= 700; i += 50) {
-        blocks.push(new Box(i, 100, 50, 50));
-    }
+    box1 = new Box(225, 360, 50, 50);
+    box2 = new Box(275, 360, 50, 50);
+    box3 = new Box(325, 360, 50, 50);
+    box4 = new Box(375, 360, 50, 50);
+    box5 = new Box(250, 300, 50, 50);
+    box6 = new Box(300, 300, 50, 50);
+    box7 = new Box(350, 300, 50, 50);
+    box8 = new Box(250, 250, 50, 50);
+    box9 = new Box(250, 200, 50, 50);
+    box10 = new Box(250, 150, 50, 50);
+    box11 = new Box(250, 100, 50, 50);
+    box12 = new Box(250, 50, 50, 50);
 
-    for (var i = 100; i <= 700; i += 50) {
-        blocks.push(new Box(i, 0, 50, 50));
-    }
+    box13 = new Box(350, 210, 50, 50);
+    box14 = new Box(400, 210, 50, 50);
+    box15 = new Box(450, 210, 50, 50);
+    box16 = new Box(350, 160, 50, 50);
+    box17 = new Box(350, 110, 50, 50);
+    box18 = new Box(350, 60, 50, 50);
+
 }
 
 function draw() {
@@ -43,13 +56,25 @@ function draw() {
     platform3.display();
     polygon.display();
     sling.display();
-    for (const block of blocks) {
-        block.display();
-    }
-}
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    box6.display();
+    box7.display();
+    box8.display();
+    box9.display();
+    box10.display();
+    box11.display();
+    box12.display();
 
-function mouseDragged() {
-
+    box13.display();
+    box14.display();
+    box15.display();
+    box16.display();
+    box17.display();
+    box18.display();
 }
 
 function mouseDragged() {
@@ -58,4 +83,10 @@ function mouseDragged() {
 
 function mouseReleased() {
     sling.fly();
+}
+
+function keyPressed() {
+    if (keyCode === 32) {
+        sling.attach(polygon.body);
+    }
 }
